179ca1cdfd6f688cb4630e623a91ab21192328ff
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-04-02 14:30:51 -0600

    Passing data with controller

--- web1/src/main/java/fer/web1/controllers/UserController.java
+++ web1/src/main/java/fer/web1/controllers/UserController.java
@@ -1,13 +1,17 @@
 package fer.web1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
-    @GetMapping("/details")
-    public String details () {
-        return "details";
-    };
+  @GetMapping("/details")
+  public String details (Model model) {
+    model.addAttribute("title", "Hola mundo Spring");
+    model.addAttribute("name", "Fernando");
+    model.addAttribute("lastname", "Garcia");
+
+    return "details";
+  };
 }
