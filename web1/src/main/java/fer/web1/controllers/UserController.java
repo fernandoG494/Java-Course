5fdfc960e5f72ac7cfe5b46a09290ce3f677f4fb
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-04-02 14:33:27 -0600

    Map implementation

--- web1/src/main/java/fer/web1/controllers/UserController.java
+++ web1/src/main/java/fer/web1/controllers/UserController.java
@@ -1,16 +1,16 @@
 package fer.web1.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Map;
 
 @Controller
 public class UserController {
   @GetMapping("/details")
-  public String details (Model model) {
-    model.addAttribute("title", "Hola mundo Spring");
-    model.addAttribute("name", "Fernando");
-    model.addAttribute("lastname", "Garcia");
+  public String details (Map<String, Object> model) {
+    model.put("title", "Hola mundo Spring");
+    model.put("name", "Fernando");
+    model.put("lastname", "Garcia");
 
     return "details";
   };
