a610453949c1875aa6d76b3d7afc8150a4c7ac88
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-04-04 12:49:01 -0600

    Only controller

--- webapp/src/main/java/fer/webapp/controllers/UserRestController.java
+++ webapp/src/main/java/fer/webapp/controllers/UserRestController.java
@@ -1,16 +1,18 @@
 package fer.webapp.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 public class UserRestController {
   @GetMapping("/details")
+  @ResponseBody
   public Map<String, Object> details() {
     Map<String, Object> body = new HashMap<>();
 
