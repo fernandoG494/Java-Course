54e2a3b122a32de060786e4eb50dc2a9d279dd09
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-04-04 12:48:04 -0600

    Rest controller update

--- webapp/src/main/java/fer/webapp/controllers/UserRestController.java
+++ webapp/src/main/java/fer/webapp/controllers/UserRestController.java
@@ -1,14 +1,16 @@
 package fer.webapp.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
-  @GetMapping("/details2")
+  @GetMapping("/details")
   public Map<String, Object> details() {
     Map<String, Object> body = new HashMap<>();
 
