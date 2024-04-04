db2322b27d89ff78f7a454e1e1217be9048b7f2c
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-04-04 12:51:16 -0600

    Request mapping with other options

--- webapp/src/main/java/fer/webapp/controllers/UserRestController.java
+++ webapp/src/main/java/fer/webapp/controllers/UserRestController.java
@@ -1,18 +1,18 @@
 package fer.webapp.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class UserRestController {
-  @GetMapping("/details")
-  @ResponseBody
+  // @GetMapping("/details")
+  @RequestMapping(path = "/details", method = RequestMethod.GET)
   public Map<String, Object> details() {
     Map<String, Object> body = new HashMap<>();
 
